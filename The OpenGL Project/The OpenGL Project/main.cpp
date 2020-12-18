d4bc31535dea3f5d961fe9e229b345d140f7cf90
Brian Montgomery <brian@montgomerycrew.net>
2020-12-18 00:46:12 -0800

    335 Final Project Commit

--- The OpenGL Project/The OpenGL Project/main.cpp
+++ The OpenGL Project/The OpenGL Project/main.cpp
@@ -11,29 +11,173 @@
 #include "Models.h"
 
 #include <iostream>
+#include <array>
+#include <chrono> 
+#include <random>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow *window, bool &fancy);
+void processInput(GLFWwindow *window);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
-bool firstMouse = true;
 
 // timing
 float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
 
-// lighting
-bool fancy = 1;
+
+
+class RectangleList {
+private:
+	class Rectangle {
+	private:
+		glm::vec3 color;
+		std::array<glm::vec3, 4> rectVerts;
+	public:
+		Rectangle() {
+			color = { 1.0f, 1.0f, 1.0f };
+			rectVerts = { glm::vec3{ 1.0f, -1.0f, 0.0f }, glm::vec3{ 1.0f, -1.0f, 0.0f }, glm::vec3{ 1.0f, -1.0f, 0.0f }, glm::vec3{ 1.0f, -1.0f, 0.0f } };
+		};
+		Rectangle(float newHeight) {
+			color = { 1.0f, 1.0f, 1.0f };
+			rectVerts = { glm::vec3{ -1.0f, newHeight, 0.0f }, glm::vec3{ 1.0f, newHeight, 0.0f }, glm::vec3{ 1.0f, -1.0f, 0.0f }, glm::vec3{ 1.0f, -1.0f, 0.0f } };
+		};
+		~Rectangle() {};
+
+		bool operator<(const Rectangle &other) {
+			if (rectVerts[0].y < other.getVert(0).y) {
+				return true;
+			}
+			return false;
+		}
+
+		void setRectangleHeight(float newHeight) {
+			rectVerts[0].y = newHeight;
+			rectVerts[1].y = newHeight;
+		}
+
+		void setColor(glm::vec3 newColor) {
+			color = newColor;
+		}
+
+		void setRectangleXPos(float newXPos, float width) {
+			rectVerts[0].x = newXPos - (width / 2);
+			rectVerts[1].x = newXPos + (width / 2);
+			rectVerts[2].x = newXPos - (width / 2);
+			rectVerts[3].x = newXPos + (width / 2);
+		}
+
+		glm::vec3 getVert(int i) const {
+			if (i >= 0 && i < 4) {
+				return rectVerts[i];
+			}
+			return glm::vec3();
+		}
+		glm::vec3 getColor() const {
+			return color;
+		}
+	};
+
+	std::array<Rectangle, 100> rectList;
+	float useScreenWidth = 1.96f;
+	float useScreenHeight = 1.8f;
+	float rectWidth = (2.0f/rectList.size());
+
+public:
+	RectangleList() {
+		for (int i = 0; i < rectList.size(); i++) {
+			rectList[i] = Rectangle(((useScreenHeight / rectList.size()) * (i + 1)) - 0.88f);
+		}
+		shuffle();
+
+		for (int i = 0; i < rectList.size(); i++) {
+			//setLocation based off of order in array
+			rectList[i].setRectangleXPos(((useScreenWidth / rectList.size()) * i + 1) - (1.98f), rectWidth);
+		}
+	}
+
+	//shuffle
+	void shuffle() {
+		unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+
+		std::shuffle(rectList.begin(), rectList.end(), std::default_random_engine(seed));
+	}
+
+	void swap(int first, int second, int &lastFirst, int &lastSecond) {
+		rectList[first].setColor(glm::vec3(1.0f, 1.0f, 0.0f));
+		rectList[second].setColor(glm::vec3(1.0f, 1.0f, 0.0f));
+		rectList[lastFirst].setColor(glm::vec3(1.0f, 1.0f, 1.0f));
+		rectList[lastSecond].setColor(glm::vec3(1.0f, 1.0f, 1.0f));
+
+		Rectangle temp = rectList[first];
+		rectList[first] = rectList[second];
+		rectList[second] = temp;
+
+		rectList[first].setRectangleXPos(((useScreenWidth / rectList.size()) * first + 1) - (1.98f), rectWidth);
+		rectList[second].setRectangleXPos(((useScreenWidth / rectList.size()) * second + 1) - (1.98f), rectWidth);
+
+		lastFirst = first;
+		lastSecond = second;
+	}
+
+	bool less(int first, int second) {
+		if (rectList[first] < rectList[second]) {
+			return true;
+		}
+		return false;
+	}
+
+	bool is_sorted(int lastSwap1, int lastSwap2) {
+		for (int i = 0; i < rectList.size()-1; i++) {
+			if (!less(i, i + 1)) {
+				return false;
+			}
+		}
+		rectList[lastSwap1].setColor(glm::vec3(1.0f, 1.0f, 1.0f));
+		rectList[lastSwap2].setColor(glm::vec3(1.0f, 1.0f, 1.0f));
+		return true;
+	}
+
+	int size() {
+		return rectList.size();
+	}
+
+	//draw
+	std::vector<float> getVertices() {
+		std::vector<float> temp;
+		for (int i = 0; i < rectList.size(); i++) {
+			for (int j = 0; j < 3; j++) {
+				temp.push_back(rectList[i].getVert(j).x);
+				temp.push_back(rectList[i].getVert(j).y);
+				temp.push_back(rectList[i].getVert(j).z);
+				temp.push_back(rectList[i].getColor().x);
+				temp.push_back(rectList[i].getColor().y);
+				temp.push_back(rectList[i].getColor().z);
+			}
+			for (int j = 1; j < 4; j++) {
+				temp.push_back(rectList[i].getVert(j).x);
+				temp.push_back(rectList[i].getVert(j).y);
+				temp.push_back(rectList[i].getVert(j).z);
+				temp.push_back(rectList[i].getColor().x);
+				temp.push_back(rectList[i].getColor().y);
+				temp.push_back(rectList[i].getColor().z);
+			}
+		}
+		return temp;
+	}
+
+	
+};
+
+bool sort(RectangleList &rectList, int &low, int &high, int &sortLoop1, int &sortLoop2, int &lastSwap1, int &lastSwap2);
 
 int main()
 {
@@ -51,7 +195,7 @@ int main()
 
 	// glfw window creation
 	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "The Grand Experiment", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Insertion Sort", NULL, NULL);
 	int curWidth = SCR_WIDTH;
 	int curHeight = SCR_HEIGHT;
 	if (window == NULL) {
@@ -68,7 +212,7 @@ int main()
 
 
 	// tell GLFW to capture our mouse
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 
 
 	// glad: load all OpenGL function pointers
@@ -87,19 +231,33 @@ int main()
 
 	// build and compile our shader zprogram
 	// ------------------------------------
-	Shader fancyShader("Shaders/lightingShader.vert", "Shaders/lightingShader.frag");
-	Shader flatShader("Shaders/lampShader.vert", "Shaders/lampShader.frag");
+	Shader twoDShader("Shaders/2DShader.vert", "Shaders/2DShader.frag");
+
+	// init the Rectangle List
+	// ------------------------------------
+	RectangleList rectList;
+	std::vector<float> vertices = rectList.getVertices();
 
 
-	//set the coords for lights to light the object
-	glm::vec3 pointLightPositions[] = {
-		glm::vec3(0.7f,  0.2f,  2.0f),
-		glm::vec3(2.3f, -3.3f, -4.0f),
-		glm::vec3(-4.0f,  2.0f, -12.0f),
-		glm::vec3(0.0f,  0.0f, -3.0f)
-	};
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
 
-	Model nanosuitModel("resources/models/nanosuit/crysis_nano_suit_2/scene.gltf");
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+	//position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	//sort variables
+	bool sorted = false;
+	int sortLow = 0, sortHigh = rectList.size(), sortLoop1 = sortLow, sortloop2 = sortLoop1, lastSwap1 = 0, lastSwap2 = 0;
 	
 	// render loop
 	// -----------
@@ -107,117 +265,42 @@ int main()
 	{
 		// per-frame time logic
 		// --------------------
-		float currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
+		float currentFrame = 0.0f;
+		while (deltaTime < 0.016f) {
+			currentFrame = glfwGetTime();
+			deltaTime = currentFrame - lastFrame;
+		}
 		lastFrame = currentFrame;
 
 		// input
 		// -----
-		processInput(window, fancy);
+		processInput(window);
+		if (!sorted) {
+			sorted = sort(rectList, sortLow, sortHigh, sortLoop1, sortloop2, lastSwap1, lastSwap2);
+			vertices = rectList.getVertices();
+			glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+		}
+
 		glfwGetFramebufferSize(window, &curWidth, &curHeight);
 
 		// render
 		// ------
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		if (fancy) {
-			// be sure to activate shader when setting uniforms/drawing objects
-			fancyShader.use();
-			fancyShader.setVec3("viewPos", camera.Position);
-			fancyShader.setFloat("material.shininess", 32.0f);
+		// render the triangle
+		twoDShader.use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, vertices.size() / 6);
 
-			/*
-			   Here we set all the uniforms for the 5/6 types of lights we have. We have to set them manually and index
-			   the proper PointLight struct in the array to set each uniform variable. This can be done more code-friendly
-			   by defining light types as classes and set their values in there, or by using a more efficient uniform approach
-			   by using 'Uniform buffer objects', but that is something we'll discuss in the 'Advanced GLSL' tutorial.
-			*/
-			// directional light
-			fancyShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
-			fancyShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-			fancyShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
-			fancyShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
-			// point light 1
-			fancyShader.setVec3("pointLights[0].position", pointLightPositions[0]);
-			fancyShader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
-			fancyShader.setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
-			fancyShader.setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
-			fancyShader.setFloat("pointLights[0].constant", 1.0f);
-			fancyShader.setFloat("pointLights[0].linear", 0.09);
-			fancyShader.setFloat("pointLights[0].quadratic", 0.032);
-			// point light 2
-			fancyShader.setVec3("pointLights[1].position", pointLightPositions[1]);
-			fancyShader.setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
-			fancyShader.setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
-			fancyShader.setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
-			fancyShader.setFloat("pointLights[1].constant", 1.0f);
-			fancyShader.setFloat("pointLights[1].linear", 0.09);
-			fancyShader.setFloat("pointLights[1].quadratic", 0.032);
-			// point light 3
-			fancyShader.setVec3("pointLights[2].position", pointLightPositions[2]);
-			fancyShader.setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
-			fancyShader.setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
-			fancyShader.setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
-			fancyShader.setFloat("pointLights[2].constant", 1.0f);
-			fancyShader.setFloat("pointLights[2].linear", 0.09);
-			fancyShader.setFloat("pointLights[2].quadratic", 0.032);
-			// point light 4
-			fancyShader.setVec3("pointLights[3].position", pointLightPositions[3]);
-			fancyShader.setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
-			fancyShader.setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
-			fancyShader.setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
-			fancyShader.setFloat("pointLights[3].constant", 1.0f);
-			fancyShader.setFloat("pointLights[3].linear", 0.09);
-			fancyShader.setFloat("pointLights[3].quadratic", 0.032);
-
-			// spotLight
-			fancyShader.setVec3("spotLight.position", camera.Position);
-			fancyShader.setVec3("spotLight.direction", camera.Front);
-			fancyShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
-			fancyShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
-			fancyShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
-			fancyShader.setFloat("spotLight.constant", 1.0f);
-			fancyShader.setFloat("spotLight.linear", 0.09);
-			fancyShader.setFloat("spotLight.quadratic", 0.032);
-			fancyShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
-			fancyShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
-
-			// view/projection transformations
-			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)curWidth / (float)curHeight, 0.1f, 100.0f);
-			glm::mat4 view = camera.GetViewMatrix();
-			fancyShader.setMat4("projection", projection);
-			fancyShader.setMat4("view", view);
-
-			// world transformation
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
-			model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// it's a bit too big for our scene, so scale it down
-
-			fancyShader.setMat4("model", model);
-
-			nanosuitModel.Draw(fancyShader);
-		} else {
-			flatShader.use();
-
-			// view/projection transformations
-			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)curWidth/ (float)curHeight, 0.1f, 100.0f);
-			glm::mat4 view = camera.GetViewMatrix();
-			flatShader.setMat4("projection", projection);
-			flatShader.setMat4("view", view);
-
-			// render the loaded model
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
-			model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// it's a bit too big for our scene, so scale it down
-			flatShader.setMat4("model", model);
-			nanosuitModel.Draw(flatShader);
-		}
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
@@ -227,7 +310,7 @@ int main()
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window, bool &fancy)
+void processInput(GLFWwindow *window)
 {
 	bool sprint = 0;
 
@@ -235,33 +318,9 @@ void processInput(GLFWwindow *window, bool &fancy)
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
-	//forward
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-		if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL)) {
-			sprint = 1;
-		}
-		camera.ProcessKeyboard(FORWARD, deltaTime, sprint);
-	}
-
-	//backwards
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, deltaTime, sprint);
-
-	//left
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, deltaTime, sprint);
-
-	//right
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, deltaTime, sprint);
-
 	//up
-	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-		camera.ProcessKeyboard(UP, deltaTime, sprint);
-
-	//down
-	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
-		camera.ProcessKeyboard(DOWN, deltaTime, sprint);
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {}
+		//restart
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -278,40 +337,54 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 // -------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
-	if (firstMouse)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
-
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-	lastX = xpos;
-	lastY = ypos;
-
-	camera.ProcessMouseMovement(xoffset, yoffset);
+	
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ----------------------------------------------------------------------
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-	camera.ProcessMouseScroll(yoffset);
+	
+}
+
+bool sort(RectangleList &rectList, int &low, int &high, int &sortLoop1, int &sortLoop2, int &lastSwap1, int &lastSwap2) {
+	bool exit = false;
+	bool nextLoop = true;
+
+	while (!exit) {
+		if (sortLoop1 <= high) {
+			if (nextLoop) {
+				sortLoop2 = sortLoop1;
+				nextLoop = false;
+			}
+			if (sortLoop2 > 0) {
+				if (rectList.less(sortLoop2, sortLoop2 - 1)) { 
+					rectList.swap(sortLoop2, sortLoop2 - 1, lastSwap1, lastSwap2); 
+					exit = true; 
+				}
+				else { 
+					sortLoop2--; 
+				}
+			}
+			else {
+				sortLoop1++;
+				nextLoop = true;
+			}
+		}
+		else {
+			sortLoop1++;
+		}
+	}
+	if (rectList.is_sorted(lastSwap1, lastSwap2)) {
+		return true;
+	}
+	return false;
 }
 
 //switches shaders
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	if (key == GLFW_KEY_F && action == GLFW_PRESS) {
-		if (fancy) {
-			fancy = 0;
-		}
-		else {
-			fancy = 1;
-		}
-	}
+	
 }
 
 
